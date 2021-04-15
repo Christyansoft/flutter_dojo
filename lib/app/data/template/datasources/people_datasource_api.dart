4b7001d95266791578a441e6f702101b33ef61ed
Lali (Naiade Lima) <naiade.lima@live.com>
2021-04-15 10:32:47 -0300

    datasource

--- lib/app/data/template/datasources/people_datasource_api.dart
+++ lib/app/data/template/datasources/people_datasource_api.dart
@@ -1,16 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
 import 'package:flutter_dojo/app/data/template/datasources/people_datasource.dart';
 import 'package:flutter_dojo/app/data/template/models/people_model.dart';
 
 class PeopleDataSourceApi implements PeopleDataSource {
+  final DioForNative _client;
+
+  PeopleDataSourceApi(this._client);
   @override
-  Future<List<PeopleModel>> getAll() {
-    // TODO: implement getAll
-    throw UnimplementedError();
+  Future<List<PeopleModel>> getAll() async {
+    final response = await _client.get('/people');
+    if (response.statusCode == 200) {
+      final result = (response.data["results"] as List)
+          .map((e) => PeopleModel.fromJson(e))
+          .toList();
+      return result;
+    }else{
+      // throw
+    }
+    
   }
 
   @override
   Future<PeopleModel> getOne(String id) {
-    // TODO: implement getOne
     throw UnimplementedError();
   }
 }
