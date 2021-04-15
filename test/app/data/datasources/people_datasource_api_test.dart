a3b8aacb4cc90dccf487d1c19a0c88eb0c263088
GManzke <gabrielmzke@gmail.com>
2021-04-15 10:58:23 -0300

    Add initial datasource unit test

--- test/app/data/datasources/people_datasource_api_test.dart
+++ test/app/data/datasources/people_datasource_api_test.dart
@@ -1,19 +1,36 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
+import 'package:flutter_dojo/app/data/template/datasources/people_datasource_api.dart';
+import 'package:flutter_dojo/app/data/template/models/people_model.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+import '../../mocks/response_mock..dart';
 
 class DioMock extends Mock implements DioForNative {}
 
 void main() {
-  // MockTemplateDatasource _datasource;
-  // PeopleRepositoryImpl _repository;
+  final dioMock = DioMock();
+  PeopleDataSourceApi datasource;
 
-  // final tTemplatemodel = PeopleModel(
-  //     height: '150', mass: '40', name: 'Teste Flutter', url: 'www.url/id/10');
+  setUp(() {
+    datasource = PeopleDataSourceApi(dioMock);
+  });
 
-  // PeopleEntity tTemplateEntity = tTemplatemodel;
+  final peopleList = (json.decode(responseAll)['results'])
+      .map((e) => PeopleModel.fromJson(e))
+      .toList();
 
-  // setUp(() {
-  //   _datasource = MockTemplateDatasource();
-  //   _repository = PeopleRepositoryImpl(_datasource);
-  // });
+  test('should return a PeopleModel from API', () async {
+    //arrange
+    when(dioMock.get(any))
+        .thenAnswer((_) async => Response(data: responseAll, statusCode: 200));
+    //act
+    final result = await datasource.getAll();
+    //assert
+    verify(dioMock.get(any)).called(1);
+    expect(result, equals(peopleList));
+  });
 }
